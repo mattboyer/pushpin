ca5804d7a8c75b25cfb6202a5e2dd6364e279475
Matt Boyer <mboyer@sdf.org>
2014-02-02 16:37:27 +0000

    Meh

--- test/test-main.js
+++ test/test-main.js
@@ -1,12 +1,17 @@
-var main = require("./main");
+var api_module = require('./pinboard_API');
 
-exports["test main"] = function(assert) {
-  assert.pass("Unit test running!");
+exports['test main'] = function(assert) {
+  assert.pass('Unit test running!');
 };
 
-exports["test main async"] = function(assert, done) {
-  assert.pass("async Unit test running!");
+exports['test main async'] = function(assert, done) {
+  assert.pass('async Unit test running!');
   done();
 };
 
-require("sdk/test").run(exports);
+
+exports['test url'] = function(assert) {
+    assert.equal(api_module.prepare_URL('posts/update', {url: 'http://www.ticalc.org'}), 'https://api.pinboard.in/v1/posts/update?url=http://www.ticalc.org&auth_token=mboyer:cdcaba2a146d12ad7a1c&format=json', 'foo');
+};
+
+require('sdk/test').run(exports);
